b217776b61f276ed59082752a9f0331d0852d403
pachecojlucas <112329254+pachecojlucas@users.noreply.github.com>
2022-11-10 09:23:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def app():
     #Gráfico de barras
     st.write("Gráfico de barras - " , label, "dentro de cada material genético")
     barra_Matgen = px.bar(tabela_resumo, x= "Irrigado",y =coluna, 
-    color='Material genético',barmode='group').update_xaxes(categoryorder="total descending")
+    color='Material genético',barmode='group').update_xaxes(categoryorder="total ascending")
 
     st.plotly_chart(barra_Matgen, use_container_width=True)
 
